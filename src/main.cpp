07ab9164a863068723aa9b1636663dc0433ec9a6
anshucodie <priyanshucodie@gmail.com>
2025-12-29 04:15:10 +0530

    now can locate executable files

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,55 @@
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include <cstdlib>
 #include <string>
+#include <sys/stat.h>
+#include <filesystem>
 using namespace std;
+namespace fs = std::filesystem;
+
+// const char* path_env ="/usr/bin:/usr/local/bin";
+
+bool is_executable(const std::string& path) {
+    struct stat st;
+    if (stat(path.c_str(), &st) == 0) {
+        return (st.st_mode & S_IXUSR) || (st.st_mode & S_IXGRP) || (st.st_mode & S_IXOTH);
+    }
+    return false;
+}
+
+bool doesItExist(string diff_path, string comd){
+    for (const auto& entry : fs::directory_iterator(diff_path)) {
+        if(entry.path().filename() == fs::path(comd) ){
+          if(is_executable(entry.path())){
+            return true;
+          }
+        }
+    }
+  return false;
+}
 
 int main() {
   // Flush after every cout / std:cerr
   cout << unitbuf;
   cerr << unitbuf;
 
+  const char* path_env = getenv("PATH");
+  vector<string> diff_paths;
+  cout << path_env << endl;
+  if(path_env){
+    string path_str(path_env);
+    stringstream ss(path_str);
+    string dir;
+    while(getline(ss, dir, ':')) {
+      diff_paths.push_back(dir);
+    }
+  }
+
   while(true){
   // TODO: Uncomment the code below to pass the first stage
   cout << "$ ";
-
+  
   // Inputing users commands
   string cmd;
   getline(cin, cmd);
@@ -18,9 +57,22 @@ int main() {
   if(cmd == "exit"){
     break;
   } else if (cmd.substr(0,4) == "type") {
-    if(cmd.substr(5) == "echo" || cmd.substr(5) == "exit" || cmd.substr(5) == "type" ){
+    string comd = cmd.substr(5);
+    if( comd == "echo" || 
+        comd == "exit" ||
+        comd == "type" || 
+        comd == "PATH" )
+    {
       cout << cmd.substr(5) << " is a shell builtin" << endl;
-    } else {
+    } else if (true) {
+      for(int i = 0; i < diff_paths.size(); i++) {
+        string select_path = diff_paths[i];
+        if(doesItExist(select_path, comd)) {
+          cout << comd << " is " << select_path << endl;
+        }
+      }
+    }
+    else {
       cout << cmd.substr(5) << ": not found" << endl;
     }
   } else if (cmd.substr(0,4) == "echo") {
